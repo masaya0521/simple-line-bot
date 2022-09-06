944601ad40ef5cf5db24578bf50f14266865312b
masaya0521 <kaji0521km@gmail.com>
2022-09-06 23:45:03 +0900

    アクセストークン発行

--- jwt/main.js
+++ jwt/main.js
@@ -2,30 +2,32 @@ let jose = require("node-jose");
 
 let privateKey = `
 {
-    "p": "4h8yEw4q9VkzhXMgXZsIZVkEuZ49EmtWYk9zs0hPTa24ejjRMA6KTYh_va0GlaChO9t0MVQVuduznt-OFZyRAinr4svU4MKD2A3gTHJJCxs0xICva8rkHXqxfPwXngpb5L_xFURbXcSTzMcKckWuOpyPznAgY4XsZxw0t7ewj9E",
-    "kty": "RSA",
-    "q": "pVhBdRN5K3MEiZzU4__TsrtSBJDD_stu60m73iIvsHIrvK3Dmfl-J1zhsyOvi3NH9mVXpUimBwP8nTe-BlVM71G7_EotFHeKH1zTmBlx6AOngmrc40W2Hd__OZW0NfC_xOTvI_Ea2BNGoGtcrIGVFLTivJ4y9wAVOKA058zJ0ls",
-    "d": "ObzE_-TROJazDm-ry-8TKRBMGzwcwTK6lMFSk7n-Xp6h7cDauSdRRYnZivC1lh5plVG3I9aUmPTRbVk7nrPqOlp4WWKQ27lyLd5IogbArpXgnBSkp9Zy0lWzvOsI3gHNnYuehyksHB53FIK93t838JfDQoXUUzalNoNwAGfkTNZxT4GIXGMGzNck2Z_urOATMf8-wdad-u4a5IB2KfHugwH2kw-Zig7fbdcN4_DeKWpuigdesa48Yj_hRJRws-mVFp-xHlGJehumnM_v8FLD85ap8L1hwvBqdJQeurcLXYzZbtdp9a5GpJI7gzOTMoEdxIKlEIIbaOKv4rkkztdhoQ",
-    "e": "AQAB",
-    "use": "sig",
-    "kid": "536e453c-aa93-4449-8e90-add2608783c6",
-    "qi": "XQ2puK9LT5yimyJXlXb4nHEBzPGe3sYbaZW_gMK4iHuM8cseImwLNP8ZIeGaNx5X_hZ6ZOzkjtYJjY85fvaWa2UDGdGlEw3ZO-Nk0Qu_exBrqZgZAsua75TjpJRw01Yd1TNBx5MYuvhltJLsjW-uSjcE-rZoO74FEe9pYYeQjI4",
-    "dp": "Qq_wlK4Y_ULRbwoFAZY3Y6xdOGDyofwF_fhwpu8sdDxHq8QV7ZZcM4GOKuJcjsRQyNZv7hxeS_H_h1tnC_igy4KRjtGOdrrnJ1DwVZte72eWqF1LXv73R7pnnfS7AmELuOriruL6Dy1qaXpKGmlyeNazkq5-3tsgXUh0Q7po2AE",
     "alg": "RS256",
-    "dq": "Wj1ovDT8lLIZb-Ggby9YotuJT-SSk6UDzHZZikquLGajaD6N2qNILsOKivKXBEzOobN9uj-EHaAXZtbdZyd27cZ2CqORJvJ299b5xLFecXpNGeio1YFee7-c1BjYWfgjMZqgycT1GairizINSjkO3FY8ySSuPBBXhKgrN7eVDrE",
-    "n": "kgwP0NPaoAwhSh9iLlRaT7FSRbNsl6T5-j-bB3xAT1UbsxOJ9v06S3_54bpYlEAkjlrO-i1vmSzfSVnqFXnjWThWRvPmBDth3Ka7hQm9UXjiAvTzYxXGFjyhALqa_-DQCtdrqIhi8E4hAuSu--kGgnFKg3G-21KJuqnVzsXrClGkxbmVufx0MJjJxr1YGfkTMG8i0dovS9tnkioDAkt1knupiYk5ir_WiNy4T-70T5s3ktC5_4Uz10hS-rWeUxiihzG8G7ceg84-Kt5jKP_AgUnel-ksRyfgSJCYC9nHyz913a3ALj3Dzt7TBaxwAjlxESrdNz5RE9DNDZfPmNWRSw"
+    "d": "Oj5zZYfVp0rRo-hk0FTIvkE9TUtIBiaHJU7MpdYfKzfIkfbdIQpqvrYqLndssqq_fX3GOKVaVJOzDpH7sQpIGArkXwZ2GAgdlrDGviM8Nu0tZPOUiD9U3ROrTCfwFRq5Rl70ko957yKg5UOXlv2nLL1PeNOG_dht9iwub8hXJLEk2a_FLfuXd6oLn49ZMoJyjfOjDmaqOaArtCSOBaGePQjrxq5vPcxiv-sWrd58AhGhYyJHPx295g_N-ASBZ2_ZMaNER6R4oRYCZ5Am228ouYPDH55QfGLPm8M_lKAh3UrQgK2sytCldtFZFzjKmwIdvct6akQ8Hk6sVcv6Zd__9Q",
+    "dp": "D9gsm8PpSfWS8CdkpW4x-bHK1L9DZYKTdEf5NDhllUXKk3lPSX-re8vTdUj088VWExSBJbDpNAjIJD5PNzlMhvp9jRRbLTghjbz271nNhR-R1XqSGzi6GeWftrx-7pZy3RKiLA-bCBd_wG7N5UIXwRhWHGGECsXitzIJIq3yXUE",
+    "dq": "g_jeRHHSBqCrYACdopHz3q7F83Fld_jCnwzQnsboF4-ayRfyog9uMSFrUipcca58K5PPbnmBlMneN3nOuVjikSmKQKOxfOClW7hDfzLpcw_Xssu__kQnMJdFcuIOifgk5ainrCfMiR9xvssSyW4ByyPssxraLYsCX0GGnvyi9Hk",
+    "e": "AQAB",
+    "ext": true,
+    "key_ops": [
+      "sign"
+    ],
+    "kty": "RSA",
+    "n": "qTuiIxidNQ9WpZUU9zGVFeNKl1VQckxuy3G3-FvmNK9lqa2xQ_aIJ0fTbNthvVvw0RPL4vZhe4OrmyhbbDm85HWoEoJ0q4Uw6OEDq7GN5wjxDFHk5_cdSAmVXeez7EKyFEMQshnwmvQfcAt-5Dli6YErKLqK_mBHK4LRnmOsRebOJhlOjaR2ebZbdSHs59LFEAFQ1KddmYi-vm5jRKVMB9L5xq0H7JD9aDXufpHvk5viZ7NwORWtVG0eS42Lmr1XPiE1fTwCJ0wI7bqWoiHPCztDUA1GN7I-23eD9RdRwkIMt_mhE_UVfa6rxfEKAGX7fS5UL7ByoOIB9pSBbAXTbw",
+    "p": "3wX2iiU58dyj8Zza4HxyzE2qJYDjO0Uchznsb9AndaD7Oye9S5lYmlqSGG9ogeKhc9Ndz7pw4HBlATwC8YNJCE2qNAI2oSvrNZTuM_HAmcq4I978zLEeDg9711MBfTeAoivx2kUCLsO68BXmtho6PLS9Zneqdm3T2m7dEfvg8h0",
+    "q": "wkGPPojJy1khygWNWJy0f5zbDbn_Lg51Sv72kXEf78LWK0ytMVgn7d_rUlhyDSwTj5TjhIBtATFZisxPr6PMFJWmaLmRGliO8JmgsJNtHiithLM5ElqZzCbbVEmySX8nbixfVbavBsL0YB3oVkvSm13zBeh6xSE7ml4ZWQBPZfs",
+    "qi": "FpSdtaETeRlmi_xY-svsuJbvAPFcOxyOdaqhE-d7Nj7GAJ_qy1hQNaUsCapdONQoZ-yMS5L5YdcH1lfGrXhJzydkMukMfRh2RGLdr0XZJWA5kesVtg71Q6tyioV7z78WgTNEKAkQG6qBOHs9wIcm-Jtg9lMn2ETY9ByEjIjbKVc"
   }
 `;
 
 let header = {
   alg: "RS256",
   typ: "JWT",
-  kid: "536e453c-aa93-4449-8e90-add2608783c6",
+  kid: "46b46bbd-dd0d-4842-98a8-7e595682d49c",
 };
 
 let payload = {
-  iss: "1234567890",
-  sub: "1234567890",
+  iss: "1657450777",
+  sub: "1657450777",
   aud: "https://api.line.me/",
   exp: Math.floor(new Date().getTime() / 1000) + 60 * 30,
   token_exp: 60 * 60 * 24 * 30,
